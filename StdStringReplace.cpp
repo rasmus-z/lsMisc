3adcb72038cfdc4726123e27980ec44078aec63f
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2012-04-13 21:32:31 +0000

    iii
    git-svn-id: svn://192.168.0.99/MyUtility@3998 5ada3c48-002d-0410-850d-99865cc6fd0f

--- StdStringReplace.cpp
+++ StdStringReplace.cpp
@@ -1,11 +1,6 @@
 ////////////////////////////////////////////////////////////////////
 //http://www20.big.or.jp/~koma/dir_WEB/contents/program/012.html
-//文字列置換
-//関数名	：　Replace()
-//引数		：　string&型　src　置換対象の文字列オブジェクト
-//		：　const char*型　pszKey 置換対象単語
-//		：　const char*型　pszRepWord 置換後単語
-//戻り値	：　なし
+
 #include <windows.h>
 #include "StdStringReplace.h"
 
