4c2d3c121752e085a67bb5d1adae98480b66432d
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2013-01-29 01:43:42 +0000

    iii
    git-svn-id: svn://192.168.0.99/MyUtility@4471 5ada3c48-002d-0410-850d-99865cc6fd0f

--- MyUtilityTest/main.cpp
+++ MyUtilityTest/main.cpp
@@ -1,14 +1,26 @@
 #include <windows.h>
 #include <tchar.h>
+#include <TlHelp32.h>
 
 #include <string>
+#include <vector>
+
 
 #include "../tstring.h"
 #include "../ChangeFilenamable2.h"
 #include "../IsFileExists.h"
+#include "../ProcessList.h"
+
+#pragma comment(lib,"Shell32.lib")
+#pragma comment(lib,"comsuppw.lib")
+#pragma comment(lib,"Advapi32.lib")
+
 
 int main()
 {
+	std::vector<PROCESSENTRY32> ret;
+	GetProcessList(ret);
+
 	TCHAR szT[] = "äJî≠ã@ç\ì¡ï éëóø";
 	tstring t = ChangeFilenamable2(szT);
 
