dfc163fcea58204f964f8593df24ee4c9ec03509
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2013-12-23 13:58:17 +0000

    akita
    git-svn-id: svn://192.168.0.99/MyUtility@4681 5ada3c48-002d-0410-850d-99865cc6fd0f

--- CBool.h
+++ CBool.h
@@ -4,7 +4,7 @@ class CBool {
 	BOOL b_;
 public:
 	CBool() : b_(FALSE) {}
-	operator BOOL() { return b_; }
+	operator BOOL() const { return b_; }
 	CBool& operator=(BOOL b) {
 		b_ = b;
 		return *this;
