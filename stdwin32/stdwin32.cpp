af92729535ab6f0e54fe4e11618b9ae89c582a8a
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2015-01-20 23:08:37 +0000

    iii
    git-svn-id: svn://192.168.0.99/MyUtility@5172 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ stdwin32/stdwin32.cpp
@@ -0,0 +1,93 @@
+#include <windows.h>
+#include <stlsoft/smartptr/scoped_handle.hpp>
+#include <string>
+using namespace std;
+#include "stdwin32.h"
+
+
+namespace stdwin32 {
+
+static const wchar_t* Nil=L"";
+
+wstring stdGetModuleFileName(HINSTANCE hInst)
+{
+	LPWSTR p = NULL;
+	size_t size = 64;
+	for(;;)
+	{
+		p = (LPWSTR)realloc(p, size*sizeof(wchar_t));
+		if(GetModuleFileName(hInst, p, size) < size)
+			break;
+		size*=2;
+	}
+	
+	wstring ret=p;
+	free((void*)p);
+	return ret;
+}
+
+BOOL stdIsFullPath(LPCWSTR pD)
+{
+	if(!pD || pD[0]==0)
+		return FALSE;
+
+	if(pD[0]==L'/')
+		return TRUE;
+
+	if( !((L'a' <= pD[0] && pD[0] <+ L'z') ||
+		  (L'A' <= pD[0] && pD[0] <+ L'Z')) )
+	{
+		return FALSE;
+	}
+
+	if(pD[1]==L':')
+		return TRUE;
+
+	return FALSE;
+}
+
+
+wstring stdCombinePath(LPCWSTR pD1, LPCWSTR pD2)
+{
+	
+	if(!pD1 || !pD1[0])
+		return pD2;
+
+	if(!pD2 || !pD2[0] )
+		return pD1;
+
+	if(stdIsFullPath(pD2))
+		return pD2;
+
+	wstring ret = pD1;
+	wstring::iterator it = ret.end();
+	--it;
+	if(*it!=L'\\')
+		ret += L'\\';
+
+	ret += pD2;
+	return ret;
+}
+
+
+wstring stdGetParentDirectory(LPCWSTR pPath)
+{
+	if(!pPath || pPath[0]==0)
+		return Nil;
+
+	LPWSTR p = wcsdup(pPath);
+	stlsoft::scoped_handle<void*> ma(p, free);
+	size_t len = wcslen(p);
+	if(p[len-1]==L'\\')
+		p[len-1]=0;
+
+	LPWSTR pT = wcsrchr(p, L'\\');
+	if(!pT)
+		return Nil;
+	*pT=0;
+	return p;
+}
+
+
+
+} // namespace stdwin32
---
+++ stdwin32/stdwin32.h
@@ -0,0 +1,7 @@
+namespace stdwin32 {
+
+	std::wstring stdGetModuleFileName(HINSTANCE hInst=NULL);
+	BOOL stdIsFullPath(LPCWSTR pD);
+	std::wstring stdCombinePath(LPCWSTR pD1, LPCWSTR pD2);
+	std::wstring stdGetParentDirectory(LPCWSTR pPath);
+}
