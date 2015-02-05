9515752314dbc23518a34073f1244e64cd40c60c
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2015-02-05 03:19:48 +0000

    iii
    git-svn-id: svn://192.168.0.99/MyUtility@5237 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ IsFileOpen.cpp
@@ -0,0 +1,28 @@
+#include <windows.h>
+#include "IsFileOpen.h"
+
+
+ISFILEOPEN IsFileOpen(LPCTSTR pFile)
+{
+	HANDLE f = CreateFile(
+		pFile,
+		GENERIC_READ,
+		0, // share
+		NULL, // sec
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+
+	if(f==INVALID_HANDLE_VALUE)
+	{
+		switch(GetLastError())
+		{
+		case ERROR_SHARING_VIOLATION:return ISFILEOPEN_YES;
+		case ERROR_ACCESS_DENIED:return ISFILEOPEN_UNKNOWN;
+		}
+		return ISFILEOPEN_NO;
+	}
+
+	CloseHandle(f);
+	return ISFILEOPEN_NO;
+}
