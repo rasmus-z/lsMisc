7a3b6c6167cf7af7fa770f2fa2dda59d019b5f2c
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2015-07-31 08:47:02 +0000

    iii
    git-svn-id: svn://192.168.0.99/MyUtility@5355 5ada3c48-002d-0410-850d-99865cc6fd0f

--- stdwin32/stdwin32.h
+++ stdwin32/stdwin32.h
@@ -1,8 +1,10 @@
+#include <vector>
+#include <string>
 namespace stdwin32 {
 
 	std::wstring stdGetModuleFileName(HINSTANCE hInst=NULL);
 	BOOL stdIsFullPath(LPCWSTR pD);
 	std::wstring stdCombinePath(LPCWSTR pD1, LPCWSTR pD2);
 	std::wstring stdGetParentDirectory(LPCWSTR pPath);
-	vector<wstring> stdSplitSCedPath(LPCWSTR pPath);
+	std::vector<std::wstring> stdSplitSCedPath(LPCWSTR pPath);
 }
