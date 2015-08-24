4a387c5aa1b0ef511da550c4567408c18a4952d5
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2015-08-24 19:47:09 +0000

    iii
    git-svn-id: svn://192.168.0.99/MyUtility@5463 5ada3c48-002d-0410-850d-99865cc6fd0f

--- stlsoft/include/stlsoft/stlsoft.h
+++ stlsoft/include/stlsoft/stlsoft.h
@@ -670,6 +670,8 @@
 #  define STLSOFT_COMPILER_VERSION_STRING       "Visual C++ 11.0"
 # elif (_MSC_VER == 1800)
 #  define STLSOFT_COMPILER_VERSION_STRING       "Visual C++ 12.0"
+# elif (_MSC_VER == 1900)
+#  define STLSOFT_COMPILER_VERSION_STRING       "Visual C++ 13.0"
 # else /* ? _MSC_VER */
 #  error Currently only versions 5.0, 6.0, 7.0, 7.1, 8.0, 9.0, 10.0, 11.0, and 12.0 of the Visual C++ compiler are supported by the STLSoft libraries
 # endif /* _MSC_VER */
