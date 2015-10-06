d5bd29485b8a616f1c4083b10a98e0656a819d54
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2015-10-06 11:53:04 +0000

    ii
    git-svn-id: svn://192.168.0.99/MyUtility@5542 5ada3c48-002d-0410-850d-99865cc6fd0f

--- showballoon.cpp
+++ showballoon.cpp
@@ -40,8 +40,45 @@ static BOOL NotifyIconize(HWND hWnd,
 }
 
 	
+//static LRESULT CALLBACK WaitWindowProc(
+//   HWND   hwnd,
+//   UINT   uMsg,
+//   WPARAM wParam,
+//   LPARAM lParam
+//)
+//{
+//	return DefWindowProc(hwnd,uMsg,wParam,lParam);
+//}
 
+HWND createSimpleWindow()
+{
+	LPCTSTR pClass=TEXT("SimpleWindowForBalloon");
+	WNDCLASS wndcls = {0};
+	wndcls.style = 0; //CS_DBLCLKS | CS_HREDRAW | CS_VREDRAW;
+	wndcls.cbClsExtra = 0;
+	wndcls.cbWndExtra = 0;
+	wndcls.hInstance = GetModuleHandle(NULL);
+	wndcls.hIcon = NULL;
+	wndcls.hCursor =NULL;// LoadCursor(NULL, IDC_IBEAM);
+	wndcls.hbrBackground = NULL; //HBRUSH(COLOR_GRAYTEXT+1);
+	wndcls.lpszMenuName = NULL;
+	wndcls.lpszClassName = pClass;
+	wndcls.lpfnWndProc = DefWindowProc;
+	if(0==RegisterClass(&wndcls))
+		return NULL;
 
+	return CreateWindowEx(
+		0, // exstyle
+		pClass,
+		TEXT(""), // window name
+		0, //WS_OVERLAPPEDWINDOW,
+		0,0,0,0,
+		NULL, // parent
+		NULL, // menu
+		NULL, // inst
+		NULL // lparam
+		);
+}
 
 BOOL showballoon(HWND hWnd, 
 				 const wstring& title,
@@ -51,33 +88,40 @@ BOOL showballoon(HWND hWnd,
 				 UINT uTrayID, 
 				 BOOL bOnlyModify)
 {
-	CoInitialize(NULL);
+	// CoInitialize(NULL);
 
-	InitCommonControls();
-	HWND hBalloon = CreateWindowW(L"tooltips_class32",
-		NULL,
-		WS_POPUP|0x02|0x40|0x01,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		NULL,
-		NULL,
-		NULL,
-		NULL);
-	if(!hBalloon)
+	HWND hwndToDel = NULL;
+	if(hWnd==NULL)
 	{
-		// MessageBoxW(NULL, L"balloon",NULL,MB_ICONERROR);
-		return FALSE;
+		hWnd = createSimpleWindow();
+		hwndToDel= hWnd;
 	}
-	if(!SendMessageW(hBalloon,
-		TTM_SETTITLE, // Adds a standard icon and title string to a ToolTip    
-		1,
-		(LPARAM)L"title"))
-	{
-		// MessageBoxW(NULL, L"balloon sendmessage",NULL,MB_ICONERROR);
-		return FALSE;
-	}
+
+	//InitCommonControls();
+	//HWND hBalloon = CreateWindowW(L"tooltips_class32",
+	//	NULL,
+	//	WS_POPUP|0x02|0x40|0x01,
+	//	CW_USEDEFAULT,
+	//	CW_USEDEFAULT,
+	//	CW_USEDEFAULT,
+	//	CW_USEDEFAULT,
+	//	NULL,
+	//	NULL,
+	//	NULL,
+	//	NULL);
+	//if(!hBalloon)
+	//{
+	//	// MessageBoxW(NULL, L"balloon",NULL,MB_ICONERROR);
+	//	return FALSE;
+	//}
+	//if(!SendMessageW(hBalloon,
+	//	TTM_SETTITLE, // Adds a standard icon and title string to a ToolTip    
+	//	1,
+	//	(LPARAM)L"title"))
+	//{
+	//	return FALSE;
+	//}
+
 
 	if(!bOnlyModify)
 	{
@@ -101,8 +145,12 @@ BOOL showballoon(HWND hWnd,
 		Sleep(duration);
 		NotifyIconize(hWnd,uTrayID, NIM_DELETE, hIcon, duration,NULL, NULL);
 	}
-	DestroyWindow(hBalloon);
+//	DestroyWindow(hBalloon);
 //	DestroyIcon(ghIcon);
+
+	if(hwndToDel)
+		DestroyWindow(hwndToDel);
+
 	return TRUE;
 }
 
