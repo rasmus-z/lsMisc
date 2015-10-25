b276ed62aed357c7d321b275fb63e38ed0355738
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2015-10-25 03:17:12 +0000

    iii
    git-svn-id: svn://192.168.0.99/MyUtility@5635 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ GetOpenFile.cpp
@@ -0,0 +1,55 @@
+#include <windows.h>
+#include <tchar.h>
+#include <string.h>
+#include <string>
+using namespace std;
+
+#include"GetOpenFile.h"
+
+BOOL GetOpenFile(
+				 HWND hWnd, 
+				 LPCTSTR pFilter, 
+				 LPCTSTR pInitialDir,
+				 LPCTSTR pTitle,
+				 wstring* fullpath, wstring* path)
+{
+	OPENFILENAME ofn;
+
+	ZeroMemory( &ofn , sizeof( ofn));
+	ofn.lStructSize = sizeof ( ofn );
+	ofn.hwndOwner = hWnd;
+	ofn.Flags = 
+		OFN_DONTADDTORECENT		| 
+		OFN_ENABLESIZING		|
+		OFN_FILEMUSTEXIST		|
+		OFN_HIDEREADONLY		|
+		0;
+
+	OFNBUFFER buff;
+	if(pFilter)
+	{
+		lstrcpy(buff.pFilter_, pFilter);
+		LPTSTR p = buff.pFilter_;
+		while( *p && (p=_tcschr(p, L'|')) )
+			*p++ = 0;
+		ofn.lpstrFilter = buff.pFilter_;
+	}
+
+	ofn.lpstrFile = buff.pFile_;
+	ofn.nMaxFile = OFNBUFFERSIZE;
+
+	ofn.lpstrFileTitle = buff.pFileTitle_;
+	ofn.nMaxFileTitle = OFNBUFFERSIZE;
+
+	ofn.lpstrInitialDir = pInitialDir;
+
+	ofn.lpstrTitle = pTitle;
+	if(!GetOpenFileName( &ofn ))
+		return FALSE;
+
+	if(path)
+		*path = ofn.lpstrFileTitle;
+
+	*fullpath = ofn.lpstrFile;
+	return TRUE;
+}
---
+++ GetOpenFile.h
@@ -0,0 +1,29 @@
+#define OFNBUFFERSIZE 1024
+struct OFNBUFFER
+{
+	
+	LPTSTR pFile_;
+	LPTSTR pFileTitle_;
+	LPTSTR pFilter_;
+	OFNBUFFER():
+		pFile_(new TCHAR[OFNBUFFERSIZE]),
+		pFileTitle_(new TCHAR[OFNBUFFERSIZE]),
+		pFilter_(new TCHAR[OFNBUFFERSIZE]) {
+			pFile_[0]=0;
+			pFileTitle_[0]=0;
+			pFilter_[0]=0;
+		}
+
+	~OFNBUFFER() {
+		delete [] pFile_;
+		delete [] pFileTitle_;
+		delete [] pFilter_;
+	}
+};
+
+BOOL GetOpenFile(
+				 HWND hWnd, 
+				 LPCTSTR pFilter, 
+				 LPCTSTR pInitialDir,
+				 LPCTSTR pTitle,
+				 wstring* fullpath, wstring* path=NULL);
