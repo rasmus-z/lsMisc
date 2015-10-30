98aba3c60cadda020bf84d6dccdb316f21cf2731
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2015-10-30 09:17:49 +0000

    ii
    git-svn-id: svn://192.168.0.99/MyUtility@5655 5ada3c48-002d-0410-850d-99865cc6fd0f

--- CreateShortcutFile.cpp
+++ CreateShortcutFile.cpp
@@ -3,6 +3,66 @@
 #include <Shobjidl.h>
 #include <Objbase.h>
 #include <ShlGuid.h>
+#include <comdef.h>
+#include <shlwapi.h>
+
+static BOOL CheckShortcutFile(LPCTSTR pszShortcutFile, 
+							   LPCTSTR pszTargetFile,
+							   LPCTSTR pszCurDir,
+							   LPCTSTR pszArg,
+						       int iIconLocation)
+{
+	BOOL bFailed = TRUE;
+	HRESULT hr;
+	IShellLinkWPtr pShellLink = NULL;
+	CoInitialize(NULL);
+    hr = CoCreateInstance(CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER, IID_IShellLink, (LPVOID*)&pShellLink);
+	int bufflen = max(lstrlen(pszShortcutFile), lstrlen(pszTargetFile));
+	bufflen = max(bufflen, lstrlen(pszCurDir));
+	bufflen = max(bufflen, lstrlen(pszArg));
+	bufflen++;
+	LPTSTR pBuffer = (LPTSTR)malloc(bufflen * sizeof(TCHAR));
+	if(SUCCEEDED(hr) && pShellLink != NULL)
+	{
+		IPersistFilePtr pPFile = NULL;
+		hr = pShellLink->QueryInterface(IID_IPersistFile, (LPVOID*)&pPFile);
+		if(SUCCEEDED(hr) && pPFile != NULL)
+		{
+			hr=pPFile->Load(pszShortcutFile, 0);
+			if(SUCCEEDED(hr))
+			{
+				bFailed = FALSE;
+				WIN32_FIND_DATA wfd;
+				if(FAILED(pShellLink->GetPath(pBuffer,bufflen,&wfd,0)))
+					return FALSE;
+				
+
+				int iI=0;
+				if(FAILED(pShellLink->GetIconLocation( pBuffer, bufflen, &iI )))
+					return FALSE;
+				if(lstrcmp(pBuffer, pszTargetFile))
+					return FALSE;
+				if( iI != iIconLocation )
+					return FALSE;
+
+
+				if( FAILED(pShellLink->GetArguments(pBuffer, bufflen)) )
+					return FALSE;
+				if(lstrcmp(pBuffer,pszArg)!=0)
+					return FALSE;
+
+				
+				if(FAILED(pShellLink->GetWorkingDirectory(pBuffer,bufflen)))
+					return FALSE;
+				if( lstrcmp(pBuffer,pszCurDir) !=0)
+					return FALSE;
+			}
+		}
+	}
+	free(pBuffer);
+	CoUninitialize();
+	return !bFailed;
+}
 
 BOOL CreateShortcutFile(LPCTSTR pszShortcutFile, 
 							   LPCTSTR pszTargetFile,
@@ -12,14 +72,22 @@ BOOL CreateShortcutFile(LPCTSTR pszShortcutFile,
 {
 	BOOL bFailed = TRUE;
 	HRESULT hr;
-	IShellLink* pShellLink = NULL;
+	IShellLinkWPtr pShellLink = NULL;
+	TCHAR szTempDir[MAX_PATH] = {0};
+	DWORD dwTP = GetTempPath(MAX_PATH, szTempDir);
+	if(dwTP==0 || !PathIsDirectory(szTempDir))
+	{
+		return FALSE;
+	}
+
+	TCHAR szTempFile[MAX_PATH] = {0};
 	CoInitialize(NULL);
     hr = CoCreateInstance(CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER, IID_IShellLink, (LPVOID*)&pShellLink);
-	if(SUCCEEDED(hr) && pShellLink)
+	if(SUCCEEDED(hr) && pShellLink != NULL)
 	{
-		IPersistFile* pPFile = NULL;
+		IPersistFilePtr pPFile = NULL;
 		hr = pShellLink->QueryInterface(IID_IPersistFile, (LPVOID*)&pPFile);
-		if(SUCCEEDED(hr) && pPFile)
+		if(SUCCEEDED(hr) && pPFile != NULL)
 		{
 			hr = pShellLink->SetPath(pszTargetFile);
 			if(SUCCEEDED(hr))
@@ -32,13 +100,32 @@ BOOL CreateShortcutFile(LPCTSTR pszShortcutFile,
 
 				bFailed |= FAILED(pShellLink->SetArguments(pszArg));
 				bFailed |= FAILED(pShellLink->SetWorkingDirectory(pszCurDir));
-				bFailed |= FAILED(pPFile->Save(pszShortcutFile, TRUE));
+
+				
+				if(0==GetTempFileName(szTempDir, L"cbs", 0, szTempFile))
+					return FALSE;
+
+				bFailed |= FAILED(pPFile->Save(szTempFile, TRUE));
 			}
-			pPFile->Release();
 		}
-		pShellLink->Release();
 	}
 	CoUninitialize();
-	return !bFailed;
+	if( bFailed )
+		return FALSE;
+
+	if(!CheckShortcutFile(szTempFile, 
+							  pszTargetFile,
+							  pszCurDir,
+							  pszArg,
+						      iIconLocation))
+	{
+		DeleteFile(szTempFile);
+		return FALSE;
+	}
+
+	if(!MoveFileEx(szTempFile, pszShortcutFile, MOVEFILE_REPLACE_EXISTING))
+		return FALSE;
+
+	return TRUE;
 }
 
