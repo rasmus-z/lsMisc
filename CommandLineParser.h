7a65f689e5ccd21069f9d25429dfcb23e898b68c
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2015-11-18 14:22:09 +0000

    iii
    git-svn-id: svn://192.168.0.99/MyUtility@5701 5ada3c48-002d-0410-850d-99865cc6fd0f

--- CommandLineParser.h
+++ CommandLineParser.h
@@ -1,29 +1,186 @@
 #pragma once
 
+#include <vector>
+using namespace std;
+
+#include "tstring.h"
+
 namespace Ambiesoft {
 
-enum ArgType {
-	ArgType_Int,
-	ArgType_Uint,
-	ArgType_String,
-	ArgType_UrlEncodedUtf8String,
-};
+//enum ArgType {
+//	ArgType_Int,
+//	ArgType_Uint,
+//	ArgType_String,
+//	ArgType_UrlEncodedUtf8String,
+//};
 
 
-struct CCommandLineInfo
+struct CAvailableCommandLineInfo
 {
 	int nID_;
-	LPCSTR pOption_;
-	ArgType argtype_;
+	LPCTSTR pOption_;
+//	ArgType argtype_;
 	unsigned long argcountflag_;
+};
+
+struct CInputCommandLineInfo
+{
+	int nID_;
+	tstring option_;
+	tstring value_;
 };
 
 class CCommandLineParser
 {
-public:
-	bool construct(CCommandLineInfo* pCLI, size_t count)
+	int argc_;
+	LPTSTR* targv_;
+
+	static tstring GetToken(LPCTSTR p)
 	{
-		return true;
+		tstring ret;
+		if(!p || !p[0])
+			return ret;
+
+		bool dq=false;
+		bool sq=false;
+
+		for( ; *p ; ++p)
+		{
+			if(_istspace(*p))
+			{
+				if(!dq && !sq)
+					break;
+			}
+			else if(*p == _T('"'))
+			{
+				if(!sq)
+				{
+					dq = true;
+					continue;
+				}
+			}
+			else if(*p == _T('\''))
+			{
+				if(!dq)
+				{
+					sq = true;
+					continue;
+				}
+			}
+
+			ret += *p;
+		}
+		return ret;
+	}
+			
+	CAvailableCommandLineInfo* pCLI_;
+	size_t clicount_;
+
+	CAvailableCommandLineInfo* FindAvailableCL(LPCTSTR pOption)
+	{
+		for(int i=0 ; i < clicount_ ; ++i)
+		{
+			CAvailableCommandLineInfo* pCLI = pCLI_;
+			pCLI += i;
+			if(_tcscmp(pCLI->pOption_, pOption)==0)
+			{
+				return pCLI;
+			}
+		}
+		return NULL;
+	}
+
+	vector<CInputCommandLineInfo> inputs_;
+	vector<CInputCommandLineInfo>::iterator current_;
+public:
+	CInputCommandLineInfo* GetNext()
+	{
+		if(!current_)
+			current_ = inputs_.begin();
+		else
+			++current_;
+
+		if(current_ == inputs_.end())
+		{
+			current_=NULL;
+			return NULL;
+		}
+
+		return current_;
+	}
+
+	CCommandLineParser(int argc, LPTSTR* targv,CAvailableCommandLineInfo* pCLI, size_t count)
+	{
+		current_ = NULL;
+
+		argc_ = argc;
+		targv_ = targv;
+		pCLI_ = pCLI;
+		clicount_ = count;
+
+		for(int i=1 ; i < argc ; ++i)
+		{
+			LPCTSTR pArgv = targv[i];
+			if(!pArgv || !pArgv[0])
+				continue;
+
+			if(pArgv[0]==_T('-') || pArgv[0]==_T('/'))
+			{
+				CAvailableCommandLineInfo* pCLI = FindAvailableCL(pArgv);	
+				if(!pCLI)
+				{
+					CInputCommandLineInfo icli;
+					icli.nID_ = -1;
+					icli.option_ = pArgv;
+					inputs_.push_back(icli);
+					continue;
+				}
+
+				if(pCLI->argcountflag_ & 0x1)
+				{
+					++i;
+					if( i >= argc )
+					{
+						CInputCommandLineInfo icli;
+						icli.nID_ = pCLI->nID_;
+						icli.option_ = pArgv;
+						inputs_.push_back(icli);
+						break;
+					}
+					
+					LPCTSTR pArgv2 = targv[i];
+					CInputCommandLineInfo icli;
+					icli.nID_ = pCLI->nID_;
+					icli.option_ = pArgv;
+					icli.value_ = pArgv2;
+					inputs_.push_back(icli);
+					continue;
+				}
+			}
+			else
+			{
+				CAvailableCommandLineInfo* pCLI = FindAvailableCL(_T(""));	
+				if(!pCLI)
+				{
+					CInputCommandLineInfo icli;
+					icli.nID_ = -2;
+					icli.option_ = _T("");
+					icli.value_ = pArgv;
+					inputs_.push_back(icli);
+					continue;
+				}
+				else
+				{
+					CInputCommandLineInfo icli;
+					icli.nID_ = pCLI->nID_;
+					icli.option_ = _T("");
+					icli.value_ = pArgv;
+					inputs_.push_back(icli);
+					continue;
+
+				}
+			}
+		}
 	}
 };
 
