adacadcfbd6868533e23eb7f22b6df387cd09ccb
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2015-12-12 18:13:36 +0000

    IsDirectory is A and W
    git-svn-id: svn://192.168.0.99/MyUtility@5934 5ada3c48-002d-0410-850d-99865cc6fd0f

--- IsDirectory.h
+++ IsDirectory.h
@@ -1,5 +1,21 @@
-inline BOOL IsDirectory(LPCTSTR pDir)
+#pragma once
+#include "CreateWString.h"
+
+
+inline BOOL IsDirectoryW(LPCWSTR pDir)
 {
 	DWORD attr = GetFileAttributes(pDir);
 	return (attr != 0xFFFFFFFF) && ((attr & FILE_ATTRIBUTE_DIRECTORY) != 0);
 }
+
+inline BOOL IsDirectoryA(LPCSTR pDir)
+{
+	CREATEWSTRING(pDir);
+	return IsDirectoryW(pDirW);
+}
+
+#ifdef UNICODE
+#define IsDirectory IsDirectoryW
+#else
+#define IsDirectory IsDirectoryA
+#endif
