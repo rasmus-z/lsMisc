d90260da767a8d8654515dbd790235d7b0eed53d
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2016-11-22 12:43:59 +0000

    iii
    git-svn-id: svn://192.168.0.99/MyUtility@6353 5ada3c48-002d-0410-850d-99865cc6fd0f

--- OpenCommon.cpp
+++ OpenCommon.cpp
@@ -1,17 +1,31 @@
 #include "stdafx.h"
 #include "OpenCommon.h"
 
-BOOL OpenCommon(HWND hWnd, LPCTSTR pApp, LPCTSTR pCommand, LPCTSTR pDirectory)
+BOOL OpenCommon(HWND hWnd, LPCTSTR pApp, LPCTSTR pCommand, LPCTSTR pDirectory, HANDLE* pPHandle)
 {
 	SHELLEXECUTEINFO sei = {0};
 	sei.cbSize = sizeof(sei);
 	sei.fMask = 0;
+	if (pPHandle)
+	{
+		sei.fMask |= SEE_MASK_NOCLOSEPROCESS;
+	}
+#ifdef UNICODE
+	sei.fMask |= SEE_MASK_UNICODE;
+#endif
+
 	sei.hwnd = hWnd;
 	sei.lpFile = pApp;
 	sei.lpParameters = pCommand;
 	sei.lpDirectory = pDirectory;
 	sei.nShow = SW_SHOW;
 
-	return ShellExecuteEx(&sei);
+	BOOL ret = ShellExecuteEx(&sei);
+	if (ret && pPHandle)
+	{
+		*pPHandle = sei.hProcess;
+	}
+
+	return ret;
 }
 
--- OpenCommon.h
+++ OpenCommon.h
@@ -1,2 +1,2 @@
 #pragma once
-BOOL OpenCommon(HWND hWnd, LPCTSTR pApp, LPCTSTR pCommand=NULL, LPCTSTR pDirectory=NULL);
+BOOL OpenCommon(HWND hWnd, LPCTSTR pApp, LPCTSTR pCommand=NULL, LPCTSTR pDirectory=NULL, HANDLE* pPHandle=NULL);
