83b65f5fbe94580b309145900fe74683f937673d
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-01-14 04:11:51 +0000

    sff big chnage
    git-svn-id: svn://192.168.0.99/MyUtility@6642 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ getStdString.net.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+#include <assert.h>
+#include <vcclr.h>
+
+namespace Ambiesoft {
+
+	std::string getStdString(System::String^ s)
+	{
+		std::string ret;
+		if ( s == nullptr )
+			return ret;
+
+		pin_ptr<const wchar_t> p = PtrToStringChars(s);
+		size_t len = (s->Length+1) * sizeof(wchar_t);
+		char* pT = (char*)malloc(len);
+		size_t retutrnvalue;
+
+		if ( 0 != wcstombs_s( &retutrnvalue, 
+			pT,
+			len,
+			p,
+			len ) )
+		{
+			assert(false);
+			free(pT);
+			return ret;
+		}
+		ret = pT;
+		free(pT);
+		return ret;
+	}
+
+
+	std::wstring getStdWstring(System::String^ s)
+	{
+		std::wstring ret;
+		if ( s == nullptr )
+			return ret;
+
+		pin_ptr<const wchar_t> p = PtrToStringChars(s);
+		ret = p;
+		return ret;
+	}
+}
---
+++ getStdString.net.h
@@ -0,0 +1,7 @@
+
+namespace Ambiesoft {
+
+	std::string getStdString(System::String^ s);
+	std::wstring getStdWstring(System::String^ s);
+
+}
