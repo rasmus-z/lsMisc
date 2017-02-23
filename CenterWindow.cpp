f58723af741be29367cee654790c0eabdd3ba1c4
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-02-23 13:38:10 +0000

    Add feature of moving windows
    git-svn-id: svn://192.168.0.99/MyUtility@6751 5ada3c48-002d-0410-850d-99865cc6fd0f

--- CenterWindow.cpp
+++ CenterWindow.cpp
@@ -53,3 +53,75 @@ BOOL CenterWindow (HWND hwndChild, HWND hwndParent)
    // Set it, and return
    return SetWindowPos (hwndChild, NULL, xNew, yNew, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 }
+
+
+
+
+bool MoveWindowCommon(HWND hWnd, MOVEWINDOWTYPE type)
+{
+	if(!hWnd || !IsWindow(hWnd))
+	{
+		return false;
+	}
+
+	RECT workingarea;
+	if(!SystemParametersInfo(
+		SPI_GETWORKAREA,
+		0,
+		&workingarea,
+		0))
+	{
+		return false;
+	}
+
+	RECT rWin;
+	if(!GetWindowRect(hWnd, &rWin))
+		return false;
+
+	POINT targetPos = {0};
+
+	switch(type)
+	{
+	case MOVEWINDOW_TOPLEFT:
+		// targetPos = screenRect.Location;
+		targetPos.x = workingarea.left;
+		targetPos.y = workingarea.top;
+		break;
+	case MOVEWINDOW_TOPRIGHT:
+		// targetPos.X = screenRect.Right - curSize.Width;
+		// targetPos.Y = screenRect.Location.Y;
+		targetPos.x = workingarea.right - rWin.right - rWin.left;
+		targetPos.y = workingarea.top;
+		break;
+	case MOVEWINDOW_BOTTOMLEFT:
+		// targetPos.X = screenRect.Location.X;
+		// targetPos.Y = screenRect.Bottom - curSize.Height;
+		targetPos.x = workingarea.left;
+		targetPos.y = workingarea.bottom - rWin.bottom - rWin.top;
+
+		break;
+	case MOVEWINDOW_BOTTOMRIGHT:
+		// targetPos.X = screenRect.Right - curSize.Width;
+		// targetPos.Y = screenRect.Bottom - curSize.Height;
+		targetPos.x = workingarea.right - rWin.right - rWin.left;
+		targetPos.y = workingarea.bottom - rWin.bottom - rWin.top;
+		break;
+
+	default:
+		return false;
+	}
+
+	//if(screenRect.Contains(targetPos))
+	//{
+	//	f->Location = targetPos;
+	//}
+
+	if(!SetWindowPos(hWnd, NULL, targetPos.x, targetPos.y,
+		0,0,
+		SWP_NOSIZE|SWP_NOZORDER))
+	{
+		return false;
+	}
+
+	return true;
+}
