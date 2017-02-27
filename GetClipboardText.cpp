6c5a0fe20a858c132bf039e1e1426cca77bc4af8
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-02-27 14:37:17 +0000

    Add GetHTML
    git-svn-id: svn://192.168.0.99/MyUtility@6775 5ada3c48-002d-0410-850d-99865cc6fd0f

--- GetClipboardText.cpp
+++ GetClipboardText.cpp
@@ -4,9 +4,9 @@
 #include "GetClipboardText.h"
 
 using namespace std;
-BOOL GetClipboardTextA(HWND hWnd, string& strData)
+static BOOL getClipboardTextDataA(HWND hWnd, int cf, string& strData)
 {
-	if( !IsClipboardFormatAvailable(CF_TEXT) )
+	if( !IsClipboardFormatAvailable(cf) )
 		return FALSE;
 	if( !OpenClipboard(hWnd) )
 		return FALSE;
@@ -15,7 +15,7 @@ BOOL GetClipboardTextA(HWND hWnd, string& strData)
 		~ClipboardCloser() {::CloseClipboard();}
 	} closer;
 
-	HANDLE hData = ::GetClipboardData(CF_TEXT);
+	HANDLE hData = ::GetClipboardData(cf);
 	if( NULL == hData )
 		return FALSE;
 
@@ -29,10 +29,83 @@ BOOL GetClipboardTextA(HWND hWnd, string& strData)
 		
 	return TRUE;
 }
-
-BOOL GetClipboardTextW(HWND hWnd, wstring& strData)
+BOOL GetClipboardTextA(HWND hWnd, string& strData)
 {
-	if( !IsClipboardFormatAvailable(CF_UNICODETEXT) )
+	return getClipboardTextDataA(hWnd, CF_TEXT, strData);
+}
+
+static BOOL getHtmlPosition(LPCSTR pStr, LPCSTR pKey, int& val)
+{
+	if(!pStr || pStr[0]==0)
+		return FALSE;
+
+	if(!pKey || pKey[0]==0)
+		return FALSE;
+
+	LPCSTR pS = strstr(pStr, pKey);
+	if(!pS)
+		return FALSE;
+
+	pS += strlen(pKey);
+	BOOL zeroFilled=false;
+	string result;
+	for(; *pS!=0 && ('0' <= *pS && *pS <= '9') ; ++pS)
+	{
+		if(!zeroFilled)
+		{
+			if('0'==*pS)
+				continue;
+			zeroFilled = true;
+			result += *pS;
+		}
+		else
+		{
+			result += *pS;
+		}
+	}
+
+	val = atoi(result.c_str());
+	return TRUE;
+}
+
+BOOL GetClipboardHtmlA(HWND hWnd, string& strData)
+{
+	static int cf = RegisterClipboardFormat(L"HTML Format");
+
+	string strT;
+	if(!getClipboardTextDataA(hWnd, cf, strT))
+		return FALSE;
+
+	int start = -1;
+	if(!getHtmlPosition(strT.c_str(), "StartHTML:", start))
+		return FALSE;
+	if(start < 0)
+		return FALSE;
+
+	int end=-1;
+	if(!getHtmlPosition(strT.c_str(), "EndHTML:", end))
+		return FALSE;
+	if(end < 0)
+		return FALSE;
+
+	if(start >= end)
+		return FALSE;
+
+	strData = strT.substr(start, end-start);
+	return TRUE;
+}
+
+
+
+
+
+
+
+
+
+static BOOL getClipboardTextDataW(HWND hWnd, int cf, wstring& strData)
+{
+	if( !IsClipboardFormatAvailable(cf) )
 		return FALSE;
 	if( !OpenClipboard(hWnd) )
 		return FALSE;
@@ -41,7 +114,7 @@ BOOL GetClipboardTextW(HWND hWnd, wstring& strData)
 		~ClipboardCloser() {::CloseClipboard();}
 	} closer;
 
-	HANDLE hData = ::GetClipboardData(CF_UNICODETEXT);
+	HANDLE hData = ::GetClipboardData(cf);
 	if( NULL == hData )
 		return FALSE;
 
@@ -53,5 +126,20 @@ BOOL GetClipboardTextW(HWND hWnd, wstring& strData)
 	GlobalUnlock(hData);
 		
 		
+	return TRUE;
+}
+BOOL GetClipboardTextW(HWND hWnd, wstring& strData)
+{
+	return getClipboardTextDataW(hWnd, CF_UNICODETEXT, strData);
+}
+BOOL GetClipboardHtmlW(HWND hWnd, wstring& strData)
+{
+	string strU8;
+	if(!GetClipboardHtmlA(hWnd, strU8))
+		return FALSE;
+
+	if(!UTF8toUTF16((const LPBYTE)strU8.c_str(), strData))
+		return FALSE;
+
 	return TRUE;
 }
--- GetClipboardText.h
+++ GetClipboardText.h
@@ -4,8 +4,13 @@
 BOOL GetClipboardTextA(HWND hWnd, std::string& strData);
 BOOL GetClipboardTextW(HWND hWnd, std::wstring& strData);
 
+BOOL GetClipboardHtmlA(HWND hWnd, std::string& strData);
+BOOL GetClipboardHtmlW(HWND hWnd, std::wstring& strData);
+
 #ifdef UNICODE
 #define GetClipboardText GetClipboardTextW
+#define GetClipboardHtml GetClipboardHtmlW
 #else
 #define GetClipboardText GetClipboardTextA
+#define GetClipboardHtml GetClipboardHtmlA
 #endif
