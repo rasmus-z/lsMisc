e4c492add834742ab942d7b3249f6ce45ffea04a
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-03-23 05:31:12 +0000

    git-svn-id: svn://192.168.0.99/MyUtility@6882 5ada3c48-002d-0410-850d-99865cc6fd0f

--- CreateProcessCommon.cpp
+++ CreateProcessCommon.cpp
@@ -92,7 +92,7 @@ BOOL CreateProcessCommon(LPCTSTR pApp,
 		//STARTF_PREVENTPINNING				|| // タスクバーにピンさせない、STARTF_TITLEISAPPIDと共に使う
 		//STARTF_RUNFULLSCREEN				|| // フルスクリーン、x86コンソールでのみ有効
 		//STARTF_TITLEISAPPID				|| // lpTitleはAppUserModeIDを設定、タスクバーの同一性情報
-		//STARTF_USESHOWWINDOW				|| // SW_HIDEとかを指定
+		STARTF_USESHOWWINDOW				|| // SW_HIDEとかを指定
 		//STARTF_USESTDHANDLES				|| // ハンドル継承関係
 		0;
 	if(bHide)
@@ -107,6 +107,10 @@ BOOL CreateProcessCommon(LPCTSTR pApp,
 	if(bHide)
 	{
 		si.wShowWindow = SW_HIDE;
+	}
+	else
+	{
+		si.wShowWindow = SW_SHOWNORMAL;
 	}
 
 
