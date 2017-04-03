70ce0d8e564e4baa233b41ba0551b97958f26bd3
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-04-03 17:38:53 +0000

    add stream
    git-svn-id: svn://192.168.0.99/MyUtility@7048 5ada3c48-002d-0410-850d-99865cc6fd0f

--- tstring.h
+++ tstring.h
@@ -4,6 +4,14 @@
 
 #ifdef UNICODE
 	typedef std::wstring tstring;
+	typedef std::wifstream tifstream;
+	typedef std::wofstream tofstream;
+	std::wostream& tcoutfunc() { return std::wcout; }
+	#define tcout tcoutfunc()
 #else
 	typedef std::string tstring;
+	typedef std::ifstream tifstream;
+	typedef std::ofstream tofstream;
+	std::ostream& tcoutfunc() { return std::cout; }
+	#define tcout tcoutfunc()
 #endif
