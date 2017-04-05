60f0df17c41fbfd74e7cab580e2a8543a8b4a747
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-04-05 08:13:32 +0000

    git-svn-id: svn://192.168.0.99/MyUtility@7065 5ada3c48-002d-0410-850d-99865cc6fd0f

--- MyUtilityTest/testsqliteserialize/testsqliteserialize/testsqliteserialize.cpp
+++ MyUtilityTest/testsqliteserialize/testsqliteserialize/testsqliteserialize.cpp
@@ -3,22 +3,38 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <sstream>
 #include "../../../sqliteserialize.h"
 #include "stlsoft/smartptr/scoped_handle.hpp"
 
+std::wstring getKey()
+{
+	static int si;
+	++si;
+
+
+	std::wstringstream ss;
+
+	ss << L"KEY" << si;
+	if(si > 5)
+		si=0;
+	return ss.str();;
+}
+
 int main()
 {
-	for (int i=0 ; ; ++i)
+	for (int i=0 ; i< 1000; ++i)
 	{
 		int d = (int)GetTickCount();
-		if (!sqlWritePrivateProfileInt(L"APP", L"KEY", d, L"file.ini"))
+		std::wstring key=getKey();
+		if (!sqlWritePrivateProfileInt(L"APP", key.c_str(), d, L"file.ini"))
 		{
 			std::cerr << "WRITE ERROR" << std::endl;
 			
 		}
 
 		
-		int f = sqlGetPrivateProfileInt(L"APP", L"KEY", 0, L"file.ini");
+		int f = sqlGetPrivateProfileInt(L"APP", key.c_str(), 0, L"file.ini");
 		if(d != f)
 		{
 			std::cerr << "RACING ERROR: " << d << "!=" << f << std::endl;
