2ccc0039af0e1a49b60e600107b01abd5119ede7
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-07-22 11:54:38 +0000

    git-svn-id: svn://192.168.0.99/MyUtility@7195 5ada3c48-002d-0410-850d-99865cc6fd0f

--- CommandLineParser.cpp
+++ CommandLineParser.cpp
@@ -9,17 +9,24 @@
 
 #ifdef __linux__ 
 #elif _WIN32
-	#include <Shellapi.h>
+#include <Shellapi.h>
 #else
 #endif
 
 using std::vector;
+using std::find;
 
 namespace Ambiesoft {
 
 	COption::COption(tstring option, unsigned long acf) : hadOption_(false)
 	{
 		options_.push_back(option);
+		argcountflag_ = acf;
+	}
+	COption::COption(tstring option1, tstring option2, unsigned long acf) : hadOption_(false)
+	{
+		options_.push_back(option1);
+		options_.push_back(option2);
 		argcountflag_ = acf;
 	}
 	COption::COption(tstring option) : hadOption_(false)
@@ -73,11 +80,198 @@ namespace Ambiesoft {
 		hadOption_ = true;
 	}
 
-	bool COption::isMatchOption(const tstring& option)
+	bool COption::isMatchOption(const tstring& option) const
 	{
-		vector< tstring >::iterator cIter = find(options_.begin(), options_.end(), option);
+		vector< tstring >::const_iterator cIter = find(options_.begin(), options_.end(), option);
 		return cIter != options_.end();
 	}
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	tstring CCommandLineParser::GetToken(LPCTSTR p)
+	{
+		tstring ret;
+		if (!p || !p[0])
+			return ret;
+
+		bool dq = false;
+		bool sq = false;
+
+		for (; *p; ++p)
+		{
+			if (_istspace(*p))
+			{
+				if (!dq && !sq)
+					break;
+			}
+			else if (*p == _T('"'))
+			{
+				if (!sq)
+				{
+					dq = true;
+					continue;
+				}
+			}
+			else if (*p == _T('\''))
+			{
+				if (!dq)
+				{
+					sq = true;
+					continue;
+				}
+			}
+
+			ret += *p;
+		}
+		return ret;
+	}
+
+
+	COption* CCommandLineParser::FindAvailableCL(const tstring& option)
+	{
+		for (POPTIONARRAY::iterator it = availables_.begin(); it != availables_.end(); ++it)
+		{
+			if ((*it)->isMatchOption(option))
+			{
+				return *it;
+			}
+		}
+		return NULL;
+	}
+
+
+
+	CCommandLineParser::CCommandLineParser()
+	{
+		empty_ = true;
+	}
+	bool CCommandLineParser::isEmpty() const {
+		return empty_;
+	}
+	tstring CCommandLineParser::getUnknowOptionStrings() const {
+		tstring ret;
+		for (OPTIONARRAY::const_iterator it = unknowns_.begin(); it != unknowns_.end(); ++it)
+		{
+			ret += it->getFirstOption();
+			if (!it->hadValue())
+			{
+				ret += _T(" ");
+				ret += it->getValueStrings();
+			}
+		}
+		return ret;
+	}
+	bool CCommandLineParser::hadUnknownOption() const {
+		return !unknowns_.empty();
+	}
+	void CCommandLineParser::AddOption(COption* cli) {
+		availables_.push_back(cli);
+	}
+#ifdef _WIN32
+#ifdef UNICODE
+	void CCommandLineParser::Parse()
+	{
+		LPWSTR *szArglist;
+		int nArgs;
+
+		szArglist = CommandLineToArgvW(GetCommandLineW(), &nArgs);
+		if (NULL == szArglist)
+		{
+			return;
+		}
+		Parse(nArgs, szArglist);
+		LocalFree(szArglist);
+	}
+#endif
+#endif
+	void CCommandLineParser::Parse(int argc, LPTSTR* targv)
+	{
+		//argc_ = argc;
+		//targv_ = targv;
+
+		for (int i = 1; i < argc; ++i)
+		{
+			LPCTSTR pArgv = targv[i];
+			if (!pArgv || !pArgv[0])
+				continue;
+
+			empty_ = false;
+
+			if (pArgv[0] == _T('-') || pArgv[0] == _T('/'))
+			{
+				COption* pA = FindAvailableCL(pArgv);
+				if (!pA)
+				{
+					//CInputCommandLineInfo icli;
+					//icli.nID_ = -1;
+					//icli.option_ = pArgv;
+					unknowns_.push_back(COption(pArgv));
+					continue;
+				}
+
+				pA->setHadOption();
+
+				if (pA->argcountflag_ & 0x1)
+				{
+					++i;
+					if (i >= argc)
+					{
+						break;
+					}
+
+					LPCTSTR pArgv2 = targv[i];
+					// pA->option_ = pArgv;
+					pA->AddValue(pArgv2);
+					continue;
+				}
+			}
+			else
+			{
+				COption* pA = FindAvailableCL(_T(""));
+				if (!pA)
+				{
+					//CInputCommandLineInfo icli;
+					//icli.nID_ = -2;
+					//icli.option_ = _T("");
+					//icli.value_ = pArgv;
+					unknowns_.push_back(COption(pArgv));
+					continue;
+				}
+				else
+				{
+					//CInputCommandLineInfo icli;
+					//icli.nID_ = pA->nID_;
+					//icli.option_ = _T("");
+					pA->AddValue(pArgv);
+					// pA.push_back(icli);
+					continue;
+
+				}
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
 
 } // namespace
--- CommandLineParser.h
+++ CommandLineParser.h
@@ -24,6 +24,7 @@ class COption
 	tstring getFirstOption() const;
 public:
 	COption(tstring option, unsigned long acf);
+	COption(tstring option1, tstring option2, unsigned long acf);
 	COption(tstring option);
 
 	friend class CCommandLineParser;
@@ -39,43 +40,7 @@ public:
 
 class CCommandLineParser
 {
-	static tstring GetToken(LPCTSTR p)
-	{
-		tstring ret;
-		if(!p || !p[0])
-			return ret;
-
-		bool dq=false;
-		bool sq=false;
-
-		for( ; *p ; ++p)
-		{
-			if(_istspace(*p))
-			{
-				if(!dq && !sq)
-					break;
-			}
-			else if(*p == _T('"'))
-			{
-				if(!sq)
-				{
-					dq = true;
-					continue;
-				}
-			}
-			else if(*p == _T('\''))
-			{
-				if(!dq)
-				{
-					sq = true;
-					continue;
-				}
-			}
-
-			ret += *p;
-		}
-		return ret;
-	}
+	static tstring GetToken(LPCTSTR p);
 			
 	typedef std::vector<COption*> POPTIONARRAY;
 	typedef std::vector<COption> OPTIONARRAY;
@@ -83,129 +48,22 @@ class CCommandLineParser
 	OPTIONARRAY unknowns_;
 	bool empty_;
 
-	COption* FindAvailableCL(const tstring& option)
-	{
-		for(POPTIONARRAY::iterator it=availables_.begin() ; it != availables_.end() ; ++it)
-		{
-			if( (*it)->isMatchOption(option) )
-			{
-				return *it;
-			}
-		}
-		return NULL;
-	}
+	COption* FindAvailableCL(const tstring& option);
 
 
 public:
-	CCommandLineParser()
-	{
-		empty_ = true;
-	}
-	bool isEmpty() const {
-		return empty_;
-	}
-	tstring getUnknowOptionStrings() const {
-		tstring ret;
-		for(OPTIONARRAY::const_iterator it=unknowns_.begin() ; it != unknowns_.end() ; ++it)
-		{
-			ret += it->getFirstOption();
-			if(!it->hadValue())
-			{
-				ret += _T(" ");
-				ret += it->getValueStrings();
-			}
-		}
-		return ret;
-	}
-	bool hadUnknownOption() const {
-		return !unknowns_.empty();
-	}
-	void AddOption(COption* cli) {
-		availables_.push_back(cli);
-	}
+	CCommandLineParser();
+	bool isEmpty() const;
+	tstring getUnknowOptionStrings() const;
+	bool hadUnknownOption() const;
+	void AddOption(COption* cli);
 #ifdef _WIN32
 #ifdef UNICODE
-	void Parse()
-	{
-		LPWSTR *szArglist;
-		int nArgs;
-
-		szArglist = CommandLineToArgvW(GetCommandLineW(), &nArgs);
-		if( NULL == szArglist )
-		{
-			return;
-		}
-		Parse(nArgs, szArglist);
-		LocalFree(szArglist);
-	}
+	void Parse();
 #endif
 #endif
-	void Parse(int argc, LPTSTR* targv)
-	{
-		//argc_ = argc;
-		//targv_ = targv;
-		
-		for(int i=1 ; i < argc ; ++i)
-		{
-			LPCTSTR pArgv = targv[i];
-			if(!pArgv || !pArgv[0])
-				continue;
+	void Parse(int argc, LPTSTR* targv);
 
-			empty_ = false;
-
-			if(pArgv[0]==_T('-') || pArgv[0]==_T('/'))
-			{
-				COption* pA = FindAvailableCL(pArgv);	
-				if(!pA)
-				{
-					//CInputCommandLineInfo icli;
-					//icli.nID_ = -1;
-					//icli.option_ = pArgv;
-					unknowns_.push_back(COption(pArgv));
-					continue;
-				}
-
-				pA->setHadOption();
-
-				if(pA->argcountflag_ & 0x1)
-				{
-					++i;
-					if( i >= argc )
-					{
-						break;
-					}
-					
-					LPCTSTR pArgv2 = targv[i];
-					// pA->option_ = pArgv;
-					pA->AddValue(pArgv2);
-					continue;
-				}
-			}
-			else
-			{
-				COption* pA = FindAvailableCL(_T(""));	
-				if(!pA)
-				{
-					//CInputCommandLineInfo icli;
-					//icli.nID_ = -2;
-					//icli.option_ = _T("");
-					//icli.value_ = pArgv;
-					unknowns_.push_back(COption(pArgv));
-					continue;
-				}
-				else
-				{
-					//CInputCommandLineInfo icli;
-					//icli.nID_ = pA->nID_;
-					//icli.option_ = _T("");
-					pA->AddValue(pArgv);
-					// pA.push_back(icli);
-					continue;
-
-				}
-			}
-		}
-	}
 };
 
 
