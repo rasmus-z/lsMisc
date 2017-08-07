1e177b697ccbe2217027d44e7f37913cd6fedf24
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-08-07 00:16:55 +0000

    git-svn-id: svn://192.168.0.99/MyUtility@7330 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ MyUtilityTest/testSHMoveFile.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include <tchar.h>
+#include <vector>
+#include <string>
+
+#include "../SHMoveFile.h"
+
+using namespace std;
+
+void testSHMoveFile()
+{
+	LPCWSTR p1 = L"C:\\Users\\Bokkurin\\Desktop\\Network Emulator Toolkit x32.rar";
+	LPCWSTR p2 = L"C:\\Users\\Bokkurin\\Desktop\\Network Emulator Toolkit x64.rar";
+	LPCWSTR pT = L"\\\\Thexp\\Share";
+
+
+	//SHMoveOneFile(pT, p1);
+	//SHMoveOneFile(pT, p2);
+
+	vector<wstring> v;
+	v.push_back(p1);
+	v.push_back(p2);
+
+	int nRet;
+	SHMoveFile(pT, v, &nRet);
+}
