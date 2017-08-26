61f2aeaa3b80a0403ed99da250d22621ad25bb68
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-08-26 14:23:02 +0000

    debug macro
    git-svn-id: svn://192.168.0.99/MyUtility@7449 5ada3c48-002d-0410-850d-99865cc6fd0f

--- DebugMacro.h
+++ DebugMacro.h
@@ -1,24 +1,6 @@
 
-#include <crtdbg.h>
 
 
-
-#if _DEBUG
-	#define malloc(size) _malloc_dbg(size, _NORMAL_BLOCK, __FILE__, __LINE__ )
-	#define calloc(s1,s2) _calloc_dbg(s1, s2, _NORMAL_BLOCK, __FILE__, __LINE__ )
-	#define realloc(p,s) _realloc_dbg(p, s, _NORMAL_BLOCK, __FILE__, __LINE__)
-	#define free(p)  _free_dbg(p, _NORMAL_BLOCK)
-	inline void* operator new(size_t nSize, LPCSTR lpszFileName, int nLine)
-	{
-		return _malloc_dbg(nSize, _NORMAL_BLOCK, lpszFileName, nLine);
-	}
-	inline void operator delete(void *p)
-	{
-		_free_dbg(p, _NORMAL_BLOCK);
-	}
-	#define DEBUG_NEW new(__FILE__, __LINE__)
-	#define new DEBUG_NEW
-#endif
 
 #if defined(_DEBUG)
 	#define DASSERT(s) System::Diagnostics::Debug::Assert(!!(s))
