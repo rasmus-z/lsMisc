a61b9eb98bf83079e32391fd0cd92d655a252cb8
fVfsr <fVfsr@THEXP>
2017-11-26 19:19:20 +0900

    Add Logging

---
+++ Logging.h
@@ -0,0 +1,60 @@
+//Copyright (C) 2017 Ambiesoft All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions
+//are met:
+//1. Redistributions of source code must retain the above copyright
+//notice, this list of conditions and the following disclaimer.
+//2. Redistributions in binary form must reproduce the above copyright
+//notice, this list of conditions and the following disclaimer in the
+//documentation and/or other materials provided with the distribution.
+//
+//THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+//ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+//FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+//OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+//OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+//SUCH DAMAGE.
+
+#pragma once
+#include "StdAfx.h"
+#include <ostream>
+#include <fstream>
+
+
+namespace Ambiesoft {
+	class Logging
+	{
+		std::wofstream file_;
+	public:
+		Logging(const wstring& file) : Logging(file.c_str()){}
+
+		Logging(const wchar_t* file)
+		{
+			if (file_.is_open())
+				return;
+			file_.open(file, std::ios::app);
+		}
+
+		~Logging()
+		{
+			file_.close();
+		}
+		void Write(const wstring& s)
+		{
+			time_t now = std::time(nullptr);
+			struct tm* localNow = std::localtime(&now);
+			wchar_t buf[64];
+			wcsftime(buf, _countof(buf), L"%c", localNow);
+			file_ << buf << L"\t";
+
+			file_ << s << std::endl;
+			file_.flush();
+		}
+	};
+}
