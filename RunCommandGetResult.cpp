8bac4de87cb52e624aa4c271225ee7fac13d45e9
fjUnc <fjUnc@THEXP>
2017-12-31 05:29:50 +0900

    muzui

---
+++ RunCommandGetResult.cpp
@@ -0,0 +1,56 @@
+//Copyright (C) 2017 Ambiesoft All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions
+//are met:
+//1. Redistributions of source code must retain the above copyright
+//notice, this list of conditions and the following disclaimer.
+//2. Redistributions in binary form must reproduce the above copyright
+//notice, this list of conditions and the following disclaimer in the
+//documentation and/or other materials provided with the distribution.
+//
+//THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+//ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+//FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+//OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+//OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+//SUCH DAMAGE.
+
+#include "stdafx.h"
+
+#include "stlScopedClear.h"
+#include "RunCommandGetResult.h"
+#include "DebugNew.h"
+
+namespace Ambiesoft {
+	BOOL RunCommandGetResult(LPCWSTR pExe, LPCWSTR pArg, int* pIRetCommand, std::string* pStrRetCommand, DWORD* pdwLastError)
+	{
+		SECURITY_ATTRIBUTES sa;
+
+		ZeroMemory(&sa, sizeof(sa));
+		sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+		sa.lpSecurityDescriptor = NULL;
+		sa.bInheritHandle = TRUE;
+
+		HANDLE hPipeSourceIn = NULL;
+		HANDLE hPipeDestOut = NULL;
+		if (!CreatePipe(&hPipeSourceIn, &hPipeDestOut, &sa, 0))
+		{
+			if (pdwLastError)
+				*pdwLastError = GetLastError();
+			return FALSE;
+		}
+		STLSCOPEDFREE(hPipeSourceIn, HANDLE, CloseHandle);
+		STLSCODEDFREE(hPipeDestOut, HANDLE, CloseHandle);
+
+		DuplicateHandle(
+			GetCurrentProcess(),
+			)
+		HANDLE hPipeDestOutDup = NULL;
+	}
+}
