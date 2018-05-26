089aa377ef5d763e1a3835544bf9c259d41227c5
Unazk <Unazk@81-64>
2018-05-27 01:42:15 +0900

    no debug malloc

--- DebugNew.h
+++ DebugNew.h
@@ -2,11 +2,11 @@
 
 #ifdef _DEBUG
 
-#include <crtdbg.h>
-#define malloc(size) _malloc_dbg(size, _NORMAL_BLOCK, __FILE__, __LINE__ )
-#define calloc(s1,s2) _calloc_dbg(s1, s2, _NORMAL_BLOCK, __FILE__, __LINE__ )
-#define realloc(p,s) _realloc_dbg(p, s, _NORMAL_BLOCK, __FILE__, __LINE__)
-#define free(p)  _free_dbg(p, _NORMAL_BLOCK)
-#define new ::new(_NORMAL_BLOCK, __FILE__, __LINE__)
+//#include <crtdbg.h>
+//#define malloc(size) _malloc_dbg(size, _NORMAL_BLOCK, __FILE__, __LINE__ )
+//#define calloc(s1,s2) _calloc_dbg(s1, s2, _NORMAL_BLOCK, __FILE__, __LINE__ )
+//#define realloc(p,s) _realloc_dbg(p, s, _NORMAL_BLOCK, __FILE__, __LINE__)
+//#define free(p)  _free_dbg(p, _NORMAL_BLOCK)
+//#define new ::new(_NORMAL_BLOCK, __FILE__, __LINE__)
 
 #endif
