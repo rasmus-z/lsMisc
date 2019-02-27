0e61089977976e6f865844318ccde75d6d369396
ambiesoft <ambiesoft.trueff@gmail.com>
2019-02-27 17:30:22 +0900

    Add WindowMessageInfo

---
+++ lsMiscTest/gtestWindowMessageInfo.cpp
@@ -0,0 +1,10 @@
+﻿#include "stdafx.h"
+
+#include "../WindowMessageInfo.h"
+
+using namespace Ambiesoft;
+
+TEST(WindowMessageInfoTest, WindowMessageInfoTest)
+{
+	EXPECT_EQ(120, WindowMessageInfo<WM_MOUSEWHEEL>::delta(MAKELONG(0,120), 0));
+}
