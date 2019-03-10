4c7479e53d4bf9635568ed67dae089e48005ffb2
ambiesoft <ambiesoft.trueff@gmail.com>
2019-03-11 01:07:21 +0900

    debugmalloc

---
+++ DebugMalloc.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef _DEBUG
+
+#include <crtdbg.h>
+#define malloc(size) _malloc_dbg(size, _NORMAL_BLOCK, __FILE__, __LINE__ )
+#define calloc(s1,s2) _calloc_dbg(s1, s2, _NORMAL_BLOCK, __FILE__, __LINE__ )
+#define realloc(p,s) _realloc_dbg(p, s, _NORMAL_BLOCK, __FILE__, __LINE__)
+#define free(p)  _free_dbg(p, _NORMAL_BLOCK)
+
+
+#endif
